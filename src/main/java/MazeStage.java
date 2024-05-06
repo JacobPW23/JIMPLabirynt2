fb8d95698f372dcf222d7f07630decbdbe73128a
Jakub Próchnicki <jakub.prochnicki.stud@pw.edu.pl>
2024-05-06 21:17:44 +0200

    Dodanie uchwytu do pól labirytnu

--- src/main/java/MazeStage.java
+++ src/main/java/MazeStage.java
@@ -6,12 +6,13 @@ import java.awt.geom.*;
 public class MazeStage extends JPanel{
 	private MazeReader reader;
 	private ArrayList<String> mazeLines;
+	private Rectangle2D.Float [][] mazeFields;
 	public MazeStage(){
 	
 	reader= new MazeReader("../resources/20x20.txt");
 	reader.readMaze();
 	this.mazeLines=reader.getMazeLines();
-
+	mazeFields= new Rectangle2D.Float[mazeLines.size()][mazeLines.get(0).length()];
 	}
 
 
@@ -32,21 +33,28 @@ public class MazeStage extends JPanel{
 		float ay=yBegining;
 		float ax=xBegining;
 		
-
+		int rowCounter=0;
 		while(it.hasNext()){
 			String line=it.next();
 			for(int i=0;i<line.length();i++){
 				
 				if(line.charAt(i)=='X'){
 					
-					Rectangle2D.Float wallElem = new Rectangle2D.Float(ax,ay,wallSize,wallSize);
-					comp2D.fill(wallElem);
+					mazeFields[rowCounter][i] = new Rectangle2D.Float(ax,ay,wallSize,wallSize);
+
+					comp2D.fill(mazeFields[rowCounter][i]);
+				}
+				else{
+					
+					 mazeFields[rowCounter][i]= new Rectangle2D.Float(ax,ay,wallSize,wallSize);
+						
 				}
 				ax+=wallSize;
+				
 			}
 			ax=xBegining;
 			ay+=wallSize;
-
+        	rowCounter++;
 		}
 
 
@@ -54,8 +62,33 @@ public class MazeStage extends JPanel{
 
 	}
 
+	/*private void buildMaze(){
+		int rows=mazeLines.size();
+		int columns=mazeLines.get(0).length();
+		if(rows>columns)
+		mazeMap=new GridLayout(rows,rows);
+		else
+			mazeMap=new GridLayout(columns,columns);
 
-	
+		mazeMap=new GridLayout(rows,columns);
+		setLayout(mazeMap);
+		mazeFields= new JPanel [rows] [columns];
+		for(int i=0;i<rows;i++){
+			String line = mazeLines.get(i);
+			for(int j=0;j<columns;j++){
+
+				mazeFields[i][j]=new JPanel();
+				if(line.charAt(j)=='X')
+					mazeFields[i][j].setBackground(Color.BLACK);
+				add(mazeFields[i][j]);
+			}
+		}
+		
+
+
+
+	}
+	*/
 
 
 }
